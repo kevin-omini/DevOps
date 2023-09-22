10a84c618c3bc3703971c517a13dd4f81f84f85e
Kevinomini <kevinomini@gmail.com>
2023-09-22 21:25:27 +0100

    cleanup bash script

--- K8s-Cluster/install_kind.sh
+++ K8s-Cluster/install_kind.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-# Author: kevin Omini
+#============================================================================
+# Author: Kevin Omini
 # Date: Sep 18, 2023
-# Desc: this is a shell script that deploys kind cluster locally on Linux
-# documentation on https://kind.sigs.k8s.io/docs/user/quick-start/#installation
+# Description: This is a shell script that deploys a kind cluster locally on Linux
+# Documentation: https://kind.sigs.k8s.io/docs/user/quick-start/#installation
+#============================================================================
 
+# Define ENVs
+CLUSTER_NAME="task"
 
-# checks the harwared of my system to download the required binaries 
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# Function to check if Kind is installed
+check_kind() {
+  if ! command -v kind &> /dev/null; then
+    echo "Kind is not installed. Downloading and installing..."
+    # For AMD64 / x86_64
+    [ "$(uname -m)" = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+    # For ARM64
+    [ "$(uname -m)"  = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
 
+    # Make kind executable
+    chmod +x ./kind
 
-# Make kind to be executable 
-chmod +x ./kind
+    # Move kind to the system binaries
+    sudo mv ./kind /usr/local/bin/kind
+    
+    echo "Kind installation completed."
+  else
+    echo "Kind is already installed."
+  fi
+}
 
-# Move kind to the system binaries 
-sudo mv ./kind /usr/local/bin/kind
+# Check if Kind is installed
+check_kind
 
-# Create a k8s Cluster 
-sudo kind create cluster
+# Create a Kubernetes Cluster using Kind
+echo "Creating $CLUSTER_NAME Cluster..."
+sudo kind create cluster --name "$CLUSTER_NAME"
 
-# Copy and save Kubeconfig in ~/.kube/config
+# Wait for the cluster to be ready
+kubectl wait --for=condition=Ready nodes --all --timeout=3m
+
+# Display cluster information
+kubectl cluster-info
+
+# Copy and save Kubeconfig to ~/.kube/config
+echo "Kubeconfig saved to ~/.kube/config"
